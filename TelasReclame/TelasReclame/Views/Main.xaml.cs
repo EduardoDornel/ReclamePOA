f10b482377390829c317a703c851da2c1cc93cb9
MatheusLigabue <matheus.s.ligabue@outlook.com>
2016-05-30 17:23:04 -0300

    Inicial

---
+++ TelasReclame/TelasReclame/Views/Main.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco é documentado em http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TelasReclame.Views
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class Main : Page
+    {
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchButton.Visibility = Visibility.Collapsed;
+            PageName.Visibility = Visibility.Collapsed;
+            SearchBar.Visibility = Visibility.Visible;          
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+    }
+}
